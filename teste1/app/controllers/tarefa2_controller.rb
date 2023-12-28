1bf5f378bf3d70ebef1d9eedbd072531934788d3
AngeloSouza1 <angeloafdesouza@gmail.com>
2023-12-28 15:02:30 -0300

    chore: configurando dados para utilizacao da funcao

--- teste1/app/controllers/tarefa2_controller.rb
+++ teste1/app/controllers/tarefa2_controller.rb
@@ -1,2 +1,10 @@
 class Tarefa2Controller < ApplicationController
+    include ApplicationHelper 
+    def index
+      @result1 = valida_cpf('461.636.517-23')
+      @result2 = valida_cpf('46163651723')
+      @result3 = valida_cpf('0000000')
+      @entrada = params[:entrada_cpf]
+      @resultado = valida_cpf(@entrada) if @entrada.present?
+    end
 end
